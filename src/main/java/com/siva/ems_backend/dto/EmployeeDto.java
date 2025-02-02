c1969f47d7ddabdaf2c8519b931b2dde5528bcfb
SivaKarthik22 <sivakarthik.s.2001@gmail.com>
2025-02-02 15:08:47 +0530

    created dto and mapper classes

---
+++ src/main/java/com/siva/ems_backend/dto/EmployeeDto.java
@@ -0,0 +1,20 @@
+package com.siva.ems_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDto {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email; 
+}
+
+//dto class just has getters and setters, it doesn't have any business logic
+//this dto class object is just used as a light-weight medium to transfer data between client and server i.e. b/w service layer and repository layer
---
+++ src/main/java/com/siva/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.siva.ems_backend.mapper;
+
+import com.siva.ems_backend.dto.EmployeeDto;
+import com.siva.ems_backend.entity.Employee;
+
+//the mapper class provide the methods to convert data from DTO object to domain object(entity) and vice versa
+public class EmployeeMapper {
+    public Employee mapToEmployee (EmployeeDto employeeDtoObj){
+        return new Employee(
+            employeeDtoObj.getId(),
+            employeeDtoObj.getFirstName(),
+            employeeDtoObj.getLastName(),
+            employeeDtoObj.getEmail()
+        );
+    }
+    public EmployeeDto mapToEmployeeDto (Employee employeeObj){
+        return new EmployeeDto(
+            employeeObj.getId(),
+            employeeObj.getFirstName(),
+            employeeObj.getLastName(),
+            employeeObj.getEmail()
+        );
+    }
+}
